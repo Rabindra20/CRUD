0151e1b95dd51627765a95315ecabbb14566d63c
Rabindra Raj Sah <53372486+Rabindra20@users.noreply.github.com>
2021-08-31 13:02:09 +0545

    canActivate

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'signup',component:RegisterComponent},
   {path:'home',
-  // canActivate: [AuthGuardService] ,
+  canActivate: [AuthGuardService] ,
   component:HomeComponent},
 ];
 
